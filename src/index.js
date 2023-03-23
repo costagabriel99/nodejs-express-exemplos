0b80fc2673b52132fee3cd260a45666bc015987d
Gabriel Costa <22gabrielgc@gmail.com>
2023-03-23 15:51:49 -0300

    Criado Controllers para user e posts

--- src/index.js
+++ src/index.js
@@ -2,25 +2,20 @@ import * as express from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('GET /')
-})
+import userController from './controller/userController'
+import postController from './controller/postContoller'
 
+app.use('/user', userController)
+app.use('/post', postController)
 
-app.post('/', (req, res) => {
-  res.status(201).send('POST /')
-})
-
-app.put('/', (req, res) => {
-  res.send('PUT /')
-})
-
-app.delete('/', (req, res) => {
-  res.send('DELETE /')
-})
-
-app.patch('/', (req, res) => {
-  res.send('PATCH /')
-})
+/*
+*USUÁRIO:
+*Cadastro
+*Login
+*
+*POSTS:
+*Criar Posts
+*Listar Posts
+*/
 
 app.listen(3000, () => console.log('ONLINE http://localhost:3000/'))
