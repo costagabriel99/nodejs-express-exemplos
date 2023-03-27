5ada9c6f8dcab894e1b3798bce5851d628b38c0d
Gabriel Costa <22gabrielgc@gmail.com>
2023-03-27 18:12:55 -0300

    Criado login e autenticação com JWT

--- src/modules/user/userService.js
+++ src/modules/user/userService.js
@@ -1,13 +1,21 @@
+import { sign } from 'jsonwebtoken'
+
 let users = []
+const generateAcessToken = (data) => sign(data, 'secret')
 
 const getUsersByEmail = (searchEmail) => users.find(({email}) => email === searchEmail)
 
 export const signup = (data) => {
-    if (getUsersByEmail(data.email)) {
-        console.log ("EXISTE O EMAIL")
-    } else {
-        users.push(data)
-    }
+    if (getUsersByEmail(data.email)) throw new Error ('email_existente')
 
-    return true
+    users.push(data)
+    return generateAcessToken({ email: data.email })
+}
+
+export const login = (data) => {
+    const user = getUsersByEmail(data.email)
+    if (!user) throw new Error ('email_nao_encontrado')
+    if (user.password !== data.password) throw new Error ('senha_incorreta')
+
+    return generateAcessToken({ email: data.email })
 }
